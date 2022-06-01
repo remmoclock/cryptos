ed355b5cc4b2c7b9454194f4f8ac269bb5dcacb7
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-06-01 20:52:25 +0200

    api axios call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [coinslist, setCoinslist] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10")
+      .then((response) => {
+        console.log(response.data.coins);
+        setCoinslist(response.data.coins);
+      });
+  }, []);
+
   return (
     <div className="App">
-      salut
+      <div className="cryptoHeader">header</div>
+      <div className="cryptoDisplay">Coins</div>
     </div>
   );
 }
