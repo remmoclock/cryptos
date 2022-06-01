12a4bff25ea8c8ed975a0a9dad30b291899eff4f
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-06-02 00:22:27 +0200

    add search bar

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import Coin from "./components/Coin/Coin";
 
 function App() {
   const [coinslist, setCoinslist] = useState([]);
+  const [searchCoin, setSearchCoin] = useState("")
 
   useEffect(() => {
     axios
-      .get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10")
+      .get("https://api.coinstats.app/public/v1/coins?skip=0&limit=15")
       .then((response) => {
         console.log(response.data.coins);
         setCoinslist(response.data.coins);
       });
-  }, []);
+    }, []);
+    console.log(searchCoin);
+
+   const filteredCoins = coinslist.filter((coin) => {
+      return coin.name.toLowerCase().includes(searchCoin.toLowerCase()) 
+    })
 
   return (
     <div className="App">
@@ -21,10 +27,10 @@ function App() {
           src={coinslist && coinslist[0] && coinslist[0].icon}
           alt="header"
         />
-        <input type="text" placeholder="Bitcoin..." />
+        <input type="text" placeholder="Bitcoin..." onChange={(e) => setSearchCoin(e.target.value)} />
       </div>
       <div className="cryptoDisplay">
-        {coinslist.map((coin) => (
+        {filteredCoins.map((coin) => (
           <Coin coin={coin} key={coin.id} />
         ))}
       </div>
